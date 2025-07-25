c5e5a4a400f4ff79bc5beeda3ec718c36bd81e09
Giada <giada.bellan@live.it>
2025-07-25 18:16:33 +0200

    implemented retry util

--- src/data/auth.js
+++ src/data/auth.js
@@ -1,3 +1,5 @@
+import { retry } from "../utils/retry.js";
+
 const API_URL = import.meta.env.VITE_APP_ECOMMERCE_API_URL;
 
 if (!API_URL) {
@@ -14,55 +16,60 @@ export const me = async () => {
 
   if (!res.ok) {
     const errorData = await res.json();
+    console.log("Error fetching user data:", errorData);
     throw new Error(errorData.error || "An error occurred while signing in");
   }
 
   const data = await res.json();
-  //console.log("me data", data);
+  //console.log("me data:", data);
   return data;
 };
 
 export const signUp = async (formData) => {
-  //console.log("signup hit");
-  //console.log("baseUrl", baseUrl);
-  //console.log("formData", formData);
-  const res = await fetch(`${baseUrl}/signup`, {
-    method: "POST",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
+  return retry(async () => {
+    //console.log("signup hit");
+    //console.log("baseUrl", baseUrl);
+    //console.log("formData", formData);
+    const res = await fetch(`${baseUrl}/signup`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.error || "An error occurred while signing up");
+    }
+
+    const data = await res.json();
+
+    return data;
   });
-
-  if (!res.ok) {
-    const errorData = await res.json();
-    throw new Error(errorData.error || "An error occurred while signing up");
-  }
-
-  const data = await res.json();
-
-  return data;
 };
 
 export const signIn = async (formData) => {
-  const res = await fetch(`${baseUrl}/signin`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-    credentials: "include", // Always include credentials for cookie-based authentication in BE
+  return retry(async () => {
+    const res = await fetch(`${baseUrl}/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+      credentials: "include", // Always include credentials for cookie-based authentication in BE
+    });
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      throw new Error(errorData.error || "An error occurred while signing in");
+    }
+
+    const data = await res.json();
+
+    return data;
   });
-
-  if (!res.ok) {
-    const errorData = await res.json();
-    throw new Error(errorData.error || "An error occurred while signing in");
-  }
-
-  const data = await res.json();
-
-  return data;
 };
 
 export const signOut = async () => {
