d706a4d9b1dcd186f16c1c38cb2da0c36d540091
Giada <giada.bellan@live.it>
2025-07-25 23:46:41 +0200

    added link to category pages to home category cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router";
 import { useProducts } from "../hooks/useProductData";
 import ProductCardSmall from "../components/card-components/ProductCardSmall";
 import HeroCarousel from "../components/HeroCarousel";
@@ -62,8 +63,12 @@ const Home = () => {
           id="category-container"
           className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 [@media(min-width:1720px)]:grid-cols-4 gap-4"
         >
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Chosen for you</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/products">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Chosen for you
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -73,8 +78,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Appliances</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/category/appliances">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Appliances
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -84,8 +93,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Tv</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/category/tv">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                TV
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -95,8 +108,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Best Deals</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/deals">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Best Deals
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -106,8 +123,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Audio</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/category/audio">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Audio
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -117,8 +138,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Mobile</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/category/mobile">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Mobile
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -128,8 +153,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Laptop</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/category/laptop">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Laptop
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
@@ -139,8 +168,12 @@ const Home = () => {
               ))}
             </div>
           </div>
-          <div className="box p-2 bg-base-200">
-            <p className="text-xl font-bold text-center mb-4">Gaming</p>
+          <div className="flex flex-col p-2 bg-base-200 items-center rounded-lg">
+            <Link to="/category/gaming">
+              <button className="text-xl font-bold text-center mb-4 cursor-pointer hover:underline">
+                Gaming
+              </button>
+            </Link>
             <div
               id="cart-container"
               className="grid grid-cols-2 grid-rows-2 gap-2"
