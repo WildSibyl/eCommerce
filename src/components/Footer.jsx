289160f8ba1744f6b9aa567e0a7a499c24bcc73c
Giada <giada.bellan@live.it>
2025-07-29 14:34:16 +0200

    added credits and link to LinkedIn profile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router";
+
 const Footer = () => {
   return (
     <footer className="flex bg-base-200 mt-4 h-[50px] bottom-0 z-10 w-full">
-      <p className="pl-3 self-center">&copy; MIAOutlet 2025 🌍</p>
+      <p className="pl-3 self-center">&copy; MIAOutlet 2025 🌍 by </p>
+      <Link
+        to="https://www.linkedin.com/in/giadabellan/"
+        className="pl-2 self-center font-bold underline hover:text-blue-400"
+      >
+        Giada Bellan
+      </Link>
     </footer>
   );
 };
